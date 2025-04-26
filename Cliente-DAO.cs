8615ee0235785f8cc9b90f68b146575e14c355e1
mewrimellow <mewri@outlook.es>
2025-04-26 13:20:55 -0300

    Initial commit

---
+++ Cliente-DAO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orgEventos1_DATA
+{
+    public class Cliente_DAO
+    {
+        private readonly string _conexao;
+
+        public Cliente_DAO(string conexao)
+        {
+            _conexao = conexao;
+        }
+
+        public void IncluirCliente(Cliente cliente)
+        {
+            const string query = @"INSERT INTO Cliente";
+        }
+
+    }
+}
---
+++ Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orgEventos1_DATA
+{
+    public class Cliente
+    {
+        public int id_cliente { get; set; }
+        public string nome { get; set; }
+
+        public string cpf { get; set; }
+
+        public string telefone { get; set; }
+
+        public string email { get; set; }
+
+        public string logradouro { get; set; }
+
+        public DateTime dataNasc { get; set; }
+
+        public string numLogradouro { get; set; }
+
+        public string complemento { get; set; }
+    }
+}
