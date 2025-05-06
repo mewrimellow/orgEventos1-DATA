0ad3d607de014465b5d9cfaa000ef7225576b8c6
mewrimellow <mewri@outlook.es>
2025-05-06 16:13:59 -0300

    ajustes trabalhador conexao dao

--- Trabalhador-DAO.cs
+++ Trabalhador-DAO.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Security.Cryptography.X509Certificates;
 
 namespace orgEventos1_DATA
 {
-    internal class Trabalhador_DAO
+    public class Trabalhador_DAO
     {
+        private readonly string _conexao;
+
+        public Trabalhador_DAO(string conexao)
+        {
+            _conexao = conexao;
+        }
+
+
+
+
     }
 }
