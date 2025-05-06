baaf41fa9844e77cf35f2c7356c522ffc5dd07b0
mewrimellow <mewri@outlook.es>
2025-05-06 15:31:04 -0300

    aaa

---
+++ Trabalhador-DAO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orgEventos1_DATA
+{
+    internal class Trabalhador_DAO
+    {
+    }
+}
