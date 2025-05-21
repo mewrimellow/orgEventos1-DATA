255fd9cdb4e1e8c9003e7018ccbfcba89064707d
gui2112-rgb <guioliveirarosa2112@gmail.com>
2025-05-21 17:07:17 -0300

    a

--- Cliente-DAO.cs
+++ Cliente-DAO.cs
@@ -91,7 +91,30 @@ namespace orgEventos1_DATA
 
         }
 
+        public void ExcluirCliente(int CodigoCliente) // OBS: Renomear para 'CodigoMedico' para manter a consistência
+        {
+            const string query = "DELETE FROM Cliente WHERE id_cliente = @id_cliente";
 
+            try
+            {
+                using (var conexaoBd = new SqlConnection(_conexao))
+                using (var comando = new SqlCommand(query, conexaoBd))
+                {
+                    comando.Parameters.Add("@id_cliente", SqlDbType.Int).Value = CodigoCliente;
+
+                    conexaoBd.Open();
+                    comando.ExecuteNonQuery(); // Executa a exclusão
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception($"Erro de banco de dados ao excluir o Medico:{ex.Message}", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erro ao excluir Medico : {ex.Message}", ex);
+            }
+        }
 
 
 
